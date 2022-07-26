2cc71e8c0f067f10471f76b57a58c388ba6909fd
shonenenenene <danya.sytaev@gmail.com>
2022-07-26 18:13:32 +0300

    fixes

--- src/components/Main/BenefitsCard/BenefitsCard.tsx
+++ src/components/Main/BenefitsCard/BenefitsCard.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from 'react'
 import { IBenefitsCard } from '../../../types'
-import SvgSelector from '../../UI/SvgSelector'
 import './BenefitsCard.scss'
 
 const BenefitsCard: FC<IBenefitsCard> = ({ src, content }) => {
+
+
+
   return (
       <div className='benefits-card'>
           <div className='benefits-card-img'>
               <img src={src} />
           </div>
-          <p className='p2'>{ content }</p>
+        {(content.includes('Подписывайтесь!')) ?
+            <p className='p2'>Публикация работ победителей во ВКонтакте. <a href='' target='_blank'>Подписывайтесь!</a></p> :
+            <p className='p2'>{content}</p>
+        }
       </div>
   )
 }
